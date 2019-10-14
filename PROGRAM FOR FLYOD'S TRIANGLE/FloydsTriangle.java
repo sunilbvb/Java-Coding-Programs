ebaeb5fa6226b32b410e6315a7591eb51410a9cd
sunilbvb <50879622+sunilbvb@users.noreply.github.com>
2019-10-14 13:07:51 +0530

    Add files via upload

---
+++ PROGRAM FOR FLYOD'S TRIANGLE/FloydsTriangle.java
@@ -0,0 +1,16 @@
+class FloydsTriangle
+{
+	public static void main(String args[])
+	{
+		int n=1;
+		for(int i=1;i<5;i++)
+		{
+			for( int j=1;j<=n;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println("");
+			n++;
+		}
+	}
+}
